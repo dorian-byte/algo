6f03aeb22a1258dbf23bdc4602d881e2588b4b79
PH <79728425+dorian-byte@users.noreply.github.com>
2022-11-29 22:26:51 -0800

    Create numberOfSegmentsInAString.py

---
+++ numberOfSegmentsInAString.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/number-of-segments-in-a-string
+class Solution:
+    def countSegments(self, s):
+        segment_count = 0
+
+        for i in range(len(s)):
+            if (i == 0 or s[i-1] == ' ') and s[i] != ' ':
+                segment_count += 1
+
+        return segment_count
