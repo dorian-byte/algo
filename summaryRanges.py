03023e179f1321d13f3846be9e836826eac004af
PH <79728425+dorian-byte@users.noreply.github.com>
2022-12-18 19:41:57 -0800

    Update summaryRanges.py

--- summaryRanges.py
+++ summaryRanges.py
@@ -2,9 +2,6 @@ class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         if not nums:
 			return nums
-	# left is exclusive and right is inclusive and always right gets incremented by 1 .
-	# Then we check if nums[i+1] != right+=1, meaning up until now left-right is a range, 
-	# so add them into ans updating left & right.
 		left, right = nums[0], nums[0]
         ans = []
         for i in range(len(nums)-1):
@@ -19,7 +16,6 @@ class Solution:
                     left = nums[i+1]
                     right = nums[i+1]-1
             right+=1
-        # after the loop exhausts left and right still holds values, so adding them
         if left!=right:
             ans.append(f"{left}->{right}")
         else:
